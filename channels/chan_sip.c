00884d197504c85db1e15d7b3143920e114e7b26
P. Christeas <xrg@linux.gr>
2011-09-29 15:21:09 +0300

    chan_sip: lastms is an integer, must have value at realtime
    Since realtime only works with strings, doing "update .. SET lastms=''"
    would bork.

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -13394,7 +13394,7 @@ static void destroy_association(struct sip_peer *peer)
 
 	if (!sip_cfg.ignore_regexpire) {
 		if (peer->rt_fromcontact && sip_cfg.peer_rtupdate) {
-			ast_update_realtime(tablename, "name", peer->name, "fullcontact", "", "ipaddr", "", "port", "", "regseconds", "0", "regserver", "", "useragent", "", "lastms", "", SENTINEL);
+			ast_update_realtime(tablename, "name", peer->name, "fullcontact", "", "ipaddr", "", "port", "0", "regseconds", "0", "regserver", "", "useragent", "", "lastms", "-1", SENTINEL);
 		} else {
 			ast_db_del("SIP/Registry", peer->name);
 			ast_db_del("SIP/PeerMethods", peer->name);
